a8dd586bf81f4e11d11841663b2ce77c089fe6c0
janisaiad <janisaiad.ja@gmail.com>
2024-10-01 13:59:19 +0200

    metadata explanation

--- data/smash/orderbook data level 1/scripts/unzip_vizualize.py
+++ data/smash/orderbook data level 1/scripts/unzip_vizualize.py
@@ -2,7 +2,7 @@ import os
 import zstandard as zstd
 import json
 import matplotlib.pyplot as plt
-from dbn import DataBento  # T'as intérêt à avoir installé les libs dbn et zstandard, sinon ça va pas le faire
+from databento import DBNStore  # T'as intérêt à avoir installé les libs dbn et zstandard, sinon ça va pas le faire
 
 def decompress_zst(zst_path, extract_path):
     """Décompresse un fichier zst comme un boss."""
