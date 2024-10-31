0d16810cb98d8ea01582488f2b156655b7c49863
janisaiad <janisaiad.ja@gmail.com>
2024-10-31 20:41:21 +0100

    new env with rust microservices

--- launch.sh
+++ launch.sh
@@ -3,5 +3,5 @@ uv venv
 source .venv/bin/activate
 uv sync
 uv pip install -e .
-uv cache purge
+uv cache prune
 uv run tests/test_env.py
--- tests/test_env.py
+++ tests/test_env.py
@@ -1,1 +1,7 @@
-import QR1
+
+if __name__ == "__main__":
+    try:
+        import QR1
+        print("QR1 found ! Environment is ready.")
+    except ImportError:
+        print("QR1 not found. Please check your environment setup.")
