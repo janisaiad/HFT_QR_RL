526264e7980639a559aa0dd84815992b6ea587b2
janisaiad <janisaiad.ja@gmail.com>
2024-11-07 00:00:09 +0100

    add pytest

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     try:
         import QR1
+        
         print("QR1 found ! Environment is ready.")
     except ImportError:
         print("QR1 not found. Please check your environment setup.")
