5d512619060a05f5c4c334a357c5f9c5375fe35b
janisaiad <janisaiad.ja@gmail.com>
2024-11-19 14:32:53 +0100

    khc seccnd running

--- data/likelihood/points_stats_conditionned_spread.py
+++ data/likelihood/points_stats_conditionned_spread.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
     
     # Process all txt files for both spread types
     for spread_type in ['zero_spread', 'nonzero_spread']:
-        txt_files = glob.glob(os.path.join(txt_dir, f"GOOGL_*_{spread_type}_outliers.txt"))
+        txt_files = glob.glob(os.path.join(txt_dir, f"KHC_*_{spread_type}_outliers.txt"))
         
         for txt_file in tqdm(txt_files):
             logging.info(f"Processing {spread_type} file: {txt_file}")
