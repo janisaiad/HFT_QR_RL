a097e08484767e8909526dbb7696044f806cc85b
janisaiad <janisaiad.ja@gmail.com>
2024-11-22 18:19:21 +0100

    readme and better uv management

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -1,44 +1,23 @@
-import unittest
+import pytest
 
-class TestCudaImports(unittest.TestCase):
+class TestCudaImports:
     def test_import_cuda(self):
         try:
             import cuda
         except ImportError:
-            self.fail("Failed to import cuda")
+            pytest.fail("Failed to import cuda")
 
     def test_import_cudf(self):
         try:
             import cudf
         except ImportError:
-            self.fail("Failed to import cudf")
-'''
-    def test_import_cuml(self):
-        try:
-            import cuml
-        except ImportError:
-            self.fail("Failed to import cuml")
-
-    def test_import_cublas(self):
-        try:
-            import cublas
-        except ImportError:
-            self.fail("Failed to import cublas")
-
-    def test_import_cufft(self):
-        try:
-            import cufft
-        except ImportError:
-            self.fail("Failed to import cufft")
-
+            pytest.fail("Failed to import cudf")
     def test_import_polars(self):
         try:
             import polars
         except ImportError:
-            self.fail("Failed to import polars")
-'''
+            pytest.fail("Failed to import polars")
 
 
 if __name__ == '__main__':
-    unittest.main()
-
+    pytest.main()
