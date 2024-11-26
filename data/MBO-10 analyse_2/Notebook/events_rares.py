7dcc6dd3d6ebec7f2fadb82aebd24bbe7b1ea2c2
Edouard <edouard.laferte@orange.fr>
2024-11-26 23:53:54 +0100

    changesss_Il faut rajouter la vol 5 min et faire les correlation avec la proba d'évents rares

--- data/MBO-10 analyse_2/Notebook/events_rares.py
+++ data/MBO-10 analyse_2/Notebook/events_rares.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore", category=pd.errors.SettingWithCopyWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_rows', None)
@@ -78,8 +79,8 @@ def remove_nan_from_dico(dico):
         cleaned_dico[key] = cleaned_value_lists
     return cleaned_dico
 
-def visu(files):
-    df_final = pd.DataFrame(columns=['date', 'nombre de trades', 'variance', 'max_proba', 'min_proba', 'mean_rapport', 'intensité max', 'seuil'])
+def visu(files, symbole):
+    df_final = pd.DataFrame(columns=['date', 'nombre de trades', 'variance', 'max_proba', 'min_proba', 'variance_proba', 'intensité max', 'seuil'])
     files_csv = glob.glob(os.path.join(files, "*.csv"))
     dic = {}
     means_trades = []
@@ -87,7 +88,8 @@ def visu(files):
     for f in tqdm(files_csv):
         df = pd.read_csv(f)
         df['ts_event'] = pd.to_datetime(df['ts_event'], errors='coerce')
-        df = df[(df['ts_event'].dt.hour > 14) | ((df['ts_event'].dt.hour == 14) & (df['ts_event'].dt.minute >= 30)) & (df['ts_event'].dt.hour < 19)]
+        df = df[(df['ts_event'].dt.hour >= 15) & (df['ts_event'].dt.hour <= 18)]
+        df['ts_event'] = df['ts_event'].dt.strftime('%H:%M:%S.%f').str[:-3]
         df['time_diff'] = df['time_diff']*1.0 # pour le modif en float c'est un timedelta là
         df['imbalance'] = df['imbalance'].round(1)
         df['imbalance'] = np.where(df['side']=='A', df['imbalance'], -df['imbalance'])
@@ -104,6 +106,7 @@ def visu(files):
     all_time = pd.Series(pd.to_datetime(all_time, errors='coerce'))
     all_time = all_time.dropna()
     all_time = all_time.sort_values().reset_index(drop=True)
+    #print(all_time)
     dic = dict(sorted(dic.items()))
     means_size = []
     for i in tqdm(dic):
@@ -115,7 +118,7 @@ def visu(files):
         file = f
         df = pd.read_csv(file)
         df['ts_event'] = pd.to_datetime(df['ts_event'], errors='coerce')
-        df = df[(df['ts_event'].dt.hour > 14) | ((df['ts_event'].dt.hour == 14) & (df['ts_event'].dt.minute >= 30)) & (df['ts_event'].dt.hour < 19)]
+        df = df[(df['ts_event'].dt.hour >= 15) & (df['ts_event'].dt.hour <= 18)]
         dic_bid = {}
         df['time_diff'] = df['time_diff']*1.0 # pour le modif en float c'est un timedelta là
         df['imbalance'] = df['imbalance'].round(1)
@@ -166,63 +169,56 @@ def visu(files):
             intens.append(tab_1)
             Add.append(np.array(pd.to_datetime(tab_time)))
 
-        #print(intensities_bid)
         extreme = 0.05
         intens = np.concatenate(intens)
         indices = np.argsort(intens)
         intens = intens[indices]
-
         add = np.concatenate(Add)
         add = add[indices]
         add = add[:int(extreme*len(add))]
-
-        Size = np.concatenate(size)
-        Size = Size[indices]
-        Size = Size[:int(extreme*len(intens))]
+        for i in range (len(add)):
+            add[i] = add[i].strftime('%H:%M:%S.%f')[:-3]
+        add = pd.Series(pd.to_datetime(add, errors='coerce'))
 
         Size_tot = np.concatenate(size)
         indices = np.argsort(Size_tot)
         add_tot = np.concatenate(Add)
-
+        
         Size_tot = Size_tot[indices][::-1][:int(extreme*len(intens))]
         add_tot = add_tot[indices][::-1][:int(extreme*len(intens))]
 
-        indices = np.argsort(add_tot)
-        add_tot = add_tot[indices]
-        Size_tot = Size_tot[indices]
-        indices = np.argsort(add)
-        add = add[indices]
-        Size = Size[indices]
+        for i in range (len(add_tot)):
+            add_tot[i] = add_tot[i].strftime('%H:%M:%S.%f')[:-3]
+        add_tot = pd.Series(pd.to_datetime(add_tot, errors='coerce'))
 
         df = pd.read_csv(file)
 
         df = df[df['action'] == 'T']
         df['ts_event'] = pd.to_datetime(df['ts_event'], errors='coerce')
-        df = df[(df['ts_event'].dt.hour > 14) | ((df['ts_event'].dt.hour == 14) & (df['ts_event'].dt.minute >= 30)) & (df['ts_event'].dt.hour < 19)]
+        df = df[(df['ts_event'].dt.hour >= 15) & (df['ts_event'].dt.hour <= 18)]
 
-        start_time = pd.to_datetime(df['ts_event'].iloc[0])
+        start_time_ = pd.to_datetime(df['ts_event'].iloc[0])
         end_time = pd.to_datetime(df['ts_event'].iloc[-1])
-
-        timy = pd.date_range(start=start_time, end=end_time, periods=200)
-
+        timy = pd.date_range(start=all_time[0], end=all_time[len(all_time)-1], periods=1000)
         counts = []
         counts_size = []
-        window_size = int((len(df['ts_event']))/10)
+        window_size = 10
         for i in range(window_size, len(timy)):
             end_time = timy[i]
-            count_all_time = ((all_time >= start_time) & (all_time <= end_time)).sum()/len(files_csv)
             start_time = timy[i - window_size]
+            count_all_time = ((all_time >= start_time) & (all_time <= end_time)).sum()
             count = ((add >= start_time) & (add <= end_time)).sum()
             count_size = ((add_tot >= start_time) & (add_tot <= end_time)).sum()
-            counts.append(count/count_all_time)
-            counts_size.append(count_size/count_all_time)
+            #print(count_all_time, count)
+            counts.append(count/(count_all_time/len(files_csv)))
+            counts_size.append(count_size/(count_all_time/len(files_csv)))
             
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=timy, y=counts, mode='lines', line=dict(color='green', width=2), name='Intensités extrêmes'))
         fig.add_trace(go.Scatter(x=timy, y=counts_size, mode='lines', line=dict(color='red', width=2), name='Sizes extrêmes'))
         fig.update_layout(
             title=(
-                f"Étude du jour {timy[0].day}/{timy[0].month}/{timy[0].year}:<br>"
+                f"Étude du jour {start_time_.day}/{start_time_.month}/{start_time_.year}:<br>"
                 f"Proba d'événements extrêmes sur une sliding window de {window_size}<br>"
                 f"pour {len(add)} événements ({extreme}% plus extrême)"
             ),
@@ -232,28 +228,28 @@ def visu(files):
             margin=dict(t=50, b=110),
             showlegend=True
         )
-        fig.write_image(f"/Volumes/T9/CSV_LCID_NASDAQ_PL_proba/{timy[0].day}-{timy[0].month}-{timy[0].year}.png", format="png")
+        fig.write_image(f"/Volumes/T9/CSV_{symbole}_NASDAQ_PL_proba/{start_time_.day}-{start_time_.month}-{start_time_.year}.png", format="png")
         fig.show()
         
-        fig = go.Figure()
+        # fig = go.Figure()
 
-        fig.add_trace(go.Scatter(x=timy, y=np.array(counts_size)/np.array(counts), mode='lines', line=dict(color='red', width=2), name='Intensité/size'))
+        # fig.add_trace(go.Scatter(x=timy, y=np.array(counts_size)/np.array(counts), mode='lines', line=dict(color='red', width=2), name='Intensité/size'))
 
-        fig.update_layout(
-            title=(
-                f"Étude du jour {timy[0].day}-{timy[0].month}-{timy[0].year}:<br>"
-                f"Proba Intensité/Proba size d'événements extrêmes sur une sliding window de {window_size}<br>"
-                f"pour {len(add)} événements ({extreme}% plus extrême)"
-            ),
-            title_x=0.5,
-            title_y=0.15,
-            yaxis_title='probabilité',
-            margin=dict(t=50, b=110),
-            showlegend=True
-        )
-        fig.write_image(f"/Volumes/T9/CSV_LCID_NASDAQ_PL_rapport/{timy[0].day}-{timy[0].month}-{timy[0].year}.png", format="png")
-        df_final.loc[ou] = [f'{timy[0].day}/{timy[0].month}/{timy[0].year}',len(df[df['action'] == 'T']),np.var(df[df['action'] == 'T']['price'].to_numpy()),np.max(np.array(counts)),np.min(np.array(counts)),np.mean(np.array(counts_size)/np.array(counts)), np.min(np.array(intens)),np.mean(intens[:int(extreme*len(intens))]) ]
+        # fig.update_layout(
+        #     title=(
+        #         f"Étude du jour {start_time.day}-{start_time.month}-{start_time.year}:<br>"
+        #         f"Proba Intensité/Proba size d'événements extrêmes sur une sliding window de {window_size}<br>"
+        #         f"pour {len(add)} événements ({extreme}% plus extrême)"
+        #     ),
+        #     title_x=0.5,
+        #     title_y=0.15,
+        #     yaxis_title='probabilité',
+        #     margin=dict(t=50, b=110),
+        #     showlegend=True
+        # )
+        # fig.write_image(f"/Volumes/T9/CSV_LCID_NASDAQ_PL_rapport/{start_time.day}-{start_time.month}-{start_time.year}.png", format="png")
+        df_final.loc[ou] = [f'{start_time_.day}/{start_time_.month}/{start_time_.year}',len(df[df['action'] == 'T']),np.var(df[df['action'] == 'T']['price'].to_numpy()),np.max(np.array(counts)),np.min(np.array(counts)),np.var(counts), np.min(np.array(intens)),np.mean(intens[:int(extreme*len(intens))]) ]
         ou+=1
-    df_final.to_csv('/Volumes/T9/CSV_LCID_PL_Analyse.csv', index = False)
+    df_final.to_csv(f'/Volumes/T9/CSV_{symbole}_PL_Analyse.csv', index = False)
     return df_final
     
