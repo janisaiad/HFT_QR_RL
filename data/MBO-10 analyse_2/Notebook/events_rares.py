01c085f5e385f610406b756812b275fd32c49fb5
Edouard <edouard.laferte@orange.fr>
2024-11-26 18:38:07 +0100

    petites modifs sur le calcul de la proba

--- data/MBO-10 analyse_2/Notebook/events_rares.py
+++ data/MBO-10 analyse_2/Notebook/events_rares.py
@@ -83,6 +83,7 @@ def visu(files):
     files_csv = glob.glob(os.path.join(files, "*.csv"))
     dic = {}
     means_trades = []
+    all_time = []
     for f in tqdm(files_csv):
         df = pd.read_csv(f)
         df['ts_event'] = pd.to_datetime(df['ts_event'], errors='coerce')
@@ -93,30 +94,29 @@ def visu(files):
         df['imbalance'] = df['imbalance'].shift()
         df = df.dropna()
         df = df[df['action'] == 'T']
-        
+        all_time.extend(df['ts_event'].tolist())
         sizes = (np.unique(df['imbalance'].to_numpy())).tolist()
         sizes.sort()
         dic = dico_queue_size(sizes, dic)  # Add, Cancel, Trade
         for row in df.itertuples():
             taille = row.imbalance
             dic[taille][2].append([row.time_diff, row.size])
-
+    all_time = pd.Series(pd.to_datetime(all_time, errors='coerce'))
+    all_time = all_time.dropna()
+    all_time = all_time.sort_values().reset_index(drop=True)
     dic = dict(sorted(dic.items()))
     means_size = []
     for i in tqdm(dic):
         means_trades.append(np.mean(dic[i][2][0]))
         means_size.append(np.mean(dic[i][2][1]))
-    print(f"A quel point c'est long: {len(dic)}")
     ou = 0
     files_csv = glob.glob(os.path.join(files, "*.csv"))
     for f in tqdm(files_csv):
         file = f
         df = pd.read_csv(file)
         df['ts_event'] = pd.to_datetime(df['ts_event'], errors='coerce')
-        #file = '/Users/edouard/Desktop/EA p1  HFT/HFT_QR_RL_save/Sans titre/HFT_QR_RL/data/MBO-10 analyse_2/test_csv/xnas-itch-20240726.mbp-10.csv'
         df = df[(df['ts_event'].dt.hour > 14) | ((df['ts_event'].dt.hour == 14) & (df['ts_event'].dt.minute >= 30)) & (df['ts_event'].dt.hour < 19)]
         dic_bid = {}
-        dic_ask = {}
         df['time_diff'] = df['time_diff']*1.0 # pour le modif en float c'est un timedelta là
         df['imbalance'] = df['imbalance'].round(1)
         df['imbalance'] = np.where(df['side']=='A', df['imbalance'], -df['imbalance'])
@@ -127,8 +127,6 @@ def visu(files):
         sizes_bid = np.array((np.unique(df['imbalance'].to_numpy())).tolist())
         sizes_bid.sort()
         dic_bid = dico_queue_size(sizes_bid, dic_bid)  # Add, Cancel, Trade
-        dic_timestamp = dico_queue_size(sizes_bid, dic_bid)
-        dic_ask = dico_queue_size(sizes_ask, dic_ask)
         for row in df.itertuples():
             if row.side == 'A':
                 taille = row.imbalance
@@ -147,26 +145,12 @@ def visu(files):
                 elif row.action == 'T':
                     dic_bid[taille][2].append([row.time_diff,row.ts_event, row.size])
 
-        Add = []
-        Cancel = []
-        Trade = []
-        sizes_add = []
-        sizes_cancel = []
-        sizes_trade = []
-
-
         intensities_bid = dict(sorted(dic_bid.items()))
-
-        threshold_trade = 1000
-        threshold = 100
-
-        quarter_add = []
-        quarter_cancel = []
-        quarter_trade = []
         Add = []
         intens = []
         l = 0
         size = []
+        
         for i in intensities_bid :
             tab_1 = []
             tab_time = []
@@ -203,26 +187,6 @@ def visu(files):
         Size_tot = Size_tot[indices][::-1][:int(extreme*len(intens))]
         add_tot = add_tot[indices][::-1][:int(extreme*len(intens))]
 
-        prices = df[df['action'] == 'T']['price']
-        time = pd.to_datetime(df[df['action'] == 'T']['ts_event'])
-
-
-        prices = df[df['action'] == 'T']['price'].reset_index(drop=True)
-        time = pd.to_datetime(df[df['action'] == 'T']['ts_event']).reset_index(drop=True)
-
-        timestamps_numeric = np.array([(ts - add[0]).total_seconds() for ts in add])
-
-        # Estimer la densité des points `add`
-        density_estimator = gaussian_kde(timestamps_numeric)
-        densities = density_estimator(timestamps_numeric)
-
-        densities_normalized = (densities - np.min(densities)) / (np.max(densities) - np.min(densities))
-
-        y_min, y_max = np.min(prices) - 0.1, np.max(prices) + 0.1
-        x_values = np.repeat(add, 50)
-        y_values = np.random.uniform(y_min, y_max, size=len(x_values))
-
-        data = pd.DataFrame({'Timestamp': x_values, 'Price': y_values})
         indices = np.argsort(add_tot)
         add_tot = add_tot[indices]
         Size_tot = Size_tot[indices]
@@ -239,18 +203,19 @@ def visu(files):
         start_time = pd.to_datetime(df['ts_event'].iloc[0])
         end_time = pd.to_datetime(df['ts_event'].iloc[-1])
 
-        timy = pd.date_range(start=start_time, end=end_time, periods=len(df['ts_event']))
+        timy = pd.date_range(start=start_time, end=end_time, periods=200)
 
         counts = []
         counts_size = []
         window_size = int((len(df['ts_event']))/10)
         for i in range(window_size, len(timy)):
             end_time = timy[i]
+            count_all_time = ((all_time >= start_time) & (all_time <= end_time)).sum()/len(files_csv)
             start_time = timy[i - window_size]
             count = ((add >= start_time) & (add <= end_time)).sum()
             count_size = ((add_tot >= start_time) & (add_tot <= end_time)).sum()
-            counts.append(count/window_size)
-            counts_size.append(count_size/window_size)
+            counts.append(count/count_all_time)
+            counts_size.append(count_size/count_all_time)
             
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=timy, y=counts, mode='lines', line=dict(color='green', width=2), name='Intensités extrêmes'))
@@ -261,15 +226,9 @@ def visu(files):
                 f"Proba d'événements extrêmes sur une sliding window de {window_size}<br>"
                 f"pour {len(add)} événements ({extreme}% plus extrême)"
             ),
-            title_x=0.5,  # Center-align the title
-            title_y=0.15,    # Place the title below the plot
+            title_x=0.5,
+            title_y=0.15,
             yaxis_title='probabilité',
-            # legend=dict(
-            #     x=1,         # Position the legend in the top-right
-            #     y=1,         # Top position
-            #     xanchor='right',
-            #     yanchor='top'
-            # ),
             margin=dict(t=50, b=110),
             showlegend=True
         )
@@ -286,23 +245,14 @@ def visu(files):
                 f"Proba Intensité/Proba size d'événements extrêmes sur une sliding window de {window_size}<br>"
                 f"pour {len(add)} événements ({extreme}% plus extrême)"
             ),
-            title_x=0.5,  # Center-align the title
-            title_y=0.15,    # Place the title below the plot
+            title_x=0.5,
+            title_y=0.15,
             yaxis_title='probabilité',
-            # legend=dict(
-            #     x=1,         # Position the legend in the top-right
-            #     y=1,         # Top position
-            #     xanchor='right',
-            #     yanchor='top'
-            # ),
             margin=dict(t=50, b=110),
             showlegend=True
         )
         fig.write_image(f"/Volumes/T9/CSV_LCID_NASDAQ_PL_rapport/{timy[0].day}-{timy[0].month}-{timy[0].year}.png", format="png")
-        #fig.show()
-        #print([f'{timy[0].day}/{timy[0].month}/{timy[0].year}',len(df[df['action'] == 'T']),np.var(df[df['action'] == 'T']['price'].to_numpy()),np.max(np.array(counts)),np.min(np.array(counts)),np.mean(np.array(counts_size)/np.array(counts)), np.min(np.array(intens))])
-        df_final.loc[ou] = [f'{timy[0].day}/{timy[0].month}/{timy[0].year}',len(df[df['action'] == 'T']),np.var(df[df['action'] == 'T']['price'].to_numpy()),np.max(np.array(counts)),np.min(np.array(counts)),np.mean(np.array(counts_size)/np.array(counts)), np.min(np.array(intens)),intens[:int(extreme*len(intens))][-1] ]
-        
+        df_final.loc[ou] = [f'{timy[0].day}/{timy[0].month}/{timy[0].year}',len(df[df['action'] == 'T']),np.var(df[df['action'] == 'T']['price'].to_numpy()),np.max(np.array(counts)),np.min(np.array(counts)),np.mean(np.array(counts_size)/np.array(counts)), np.min(np.array(intens)),np.mean(intens[:int(extreme*len(intens))]) ]
         ou+=1
     df_final.to_csv('/Volumes/T9/CSV_LCID_PL_Analyse.csv', index = False)
     return df_final
